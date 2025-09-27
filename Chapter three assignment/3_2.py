f2c7e7fc4a5c4e19482ffda598772df6d4050abf
Abdulwahab Adamson <adamsonoladipupo@gmail.com>
2025-09-28 00:21:50 +0100

    new update

---
+++ Chapter three assignment/3_2.py
@@ -0,0 +1,9 @@
+a = b = 7
+print('a =', a, '\nb =', b)
+
+docstrings = """
+	The code is okay,
+	two variable are given thesame value
+
+"""
+print(docstrings)
--- Chapter three assignment/validating_user_input.py
+++ Chapter three assignment/validating_user_input.py
@@ -1,1 +1,8 @@
-validating_user_input
+number_of_failure = 0
+number = 0
+while number !=1 and number !=2 :
+	number = int(input("Enter a number: "))
+	if number != 1 and number != 2:
+		number_of_failure += 1
+
+print("number of failures", number_of_failure)
