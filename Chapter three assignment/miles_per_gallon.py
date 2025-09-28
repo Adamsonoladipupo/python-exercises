ceed585ea64f46a1915238a0331986fcac473ecb
Abdulwahab Adamson <adamsonoladipupo@gmail.com>
2025-09-28 08:57:51 +0100

    new update

--- Chapter three assignment/miles_per_gallon.py
+++ Chapter three assignment/miles_per_gallon.py
@@ -3,12 +3,14 @@ gallons = 1;
 miles = 1
 sum_of_gallon = 0
 sum_of_miles = 0
+miles_per_gallon = 0
 while gallons != -1:
 	gallons = int(input("Enter the gallons used (-1 to end): "))
-	miles = int(input("Enter the miles driven: "))
-	sum_of_gallon += gallons
-	sum_of_miles += miles
-	miles_per_gallon = miles/ gallons
-	print(f"the miles per gallon for this trip was: {miles_per_gallon}") 
+	if gallons != -1:
+		miles = int(input("Enter the miles driven: "))
+		sum_of_gallon += gallons
+		sum_of_miles += miles
+		miles_per_gallon = miles/ gallons
+		print(f"the miles per gallon for this trip was: {miles_per_gallon}") 
 overall_average = sum_of_miles/sum_of_gallon
 print(f"The overall average miles/gallon was: {overall_average}")
