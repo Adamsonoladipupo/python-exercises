439c57dd1bec108842ac9dcdc29075ec0580b55c
Abdulwahab Adamson <adamsonoladipupo@gmail.com>
2025-09-29 13:05:32 +0100

    update

---
+++ Chapter three assignment/dollar_coin.py
@@ -0,0 +1,15 @@
+number = int(input("Enter a number: "))
+docstring = '''
+
+	$1 = 100 pennies
+	$1 = 20 nickels
+	$1 = 10 dimes
+	$1 = 4 quarters
+
+'''
+pennies = number * 100
+nickels = number * 20
+dimes = number * 10
+quarters = number * 4
+
+print("Pennies: ", pennies, "Nickels: ", nickels, "Dimes: ", dimes,"Quarters: ", quarters)
