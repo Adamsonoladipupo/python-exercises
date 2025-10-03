c78e8cff20d72c4605ffd8c506b277fc3549b085
Abdulwahab Adamson <adamsonoladipupo@gmail.com>
2025-10-03 06:41:23 +0100

    update

--- Midnight Corn flakes 3_10_2025/palindrome_and_prime_number.py
+++ Midnight Corn flakes 3_10_2025/palindrome_and_prime_number.py
@@ -1,1 +1,24 @@
-def palindrome_and_prime_number (a, b):
+def palindrome_and_prime_number(a):
+
+	unith = 0
+	tenth = 0
+	hundredth = 0
+
+	if a % 10 != 0 or a // 10 != 0:
+		unith = a % 10
+		a = a // 10
+	if a % 10 != 0 or a // 10 != 0:
+		tenth = a % 10
+		a = a // 10
+	if a % 10 != 0 or a // 10 != 0:
+		a = a % 10
+		hundredth = a // 10
+
+	if unith == hundredth:
+		return print("Palindromes")
+	else:
+		return print("Not a Palindromes")
+
+
+number = int(input("Enter a number: "))
+check = palindrome_and_prime_number(number)
