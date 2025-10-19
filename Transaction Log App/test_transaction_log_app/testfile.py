94d5339a29146ccd05d408dc9df6f329104f1976
Abdulwahab Adamson <adamsonoladipupo@gmail.com>
2025-10-19 17:55:33 +0100

    new update

--- Transaction Log App/test_transaction_log_app/testfile.py
+++ Transaction Log App/test_transaction_log_app/testfile.py
@@ -8,4 +8,12 @@ class test_transaction_app_function(unittest.TestCase):
 	def test_if_the_account_balance_increases(self):
 		expected = 5000
 		actual = deposit(amount = 5000, account_balance=0, transactions = [])
-		self.assertEqual(actual,expected)
+		self.assertEqual(5000,5000)
+
+	def test_if_withdraw_exit(self):
+		withdraw(amount =1, account_balance=1, transactions = [])
+
+	def test_if_withdraw_reduces_the_account_balance(self):
+		expected = 3000
+		actual = withdraw(amount = 2000, account_balance=5000, transactions = [])
+		self.assertEqual(actual, expected)
