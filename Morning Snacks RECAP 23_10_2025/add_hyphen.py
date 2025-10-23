ffd835166edb4d08715b1882c868b9fc91e6bcc3
Abdulwahab Adamson <adamsonoladipupo@gmail.com>
2025-10-23 14:33:59 +0100

    Morning Snacks RECAP 23_10_2025

--- Morning Snacks RECAP 23_10_2025/add_hyphen.py
+++ Morning Snacks RECAP 23_10_2025/add_hyphen.py
@@ -1,5 +1,9 @@
+from functools import reduce
+
 words = ["I", "Love", "Python", "Snacks"]
 
-
 def add_hyphen(word1, word2):
-	
+	return word1 + "-" + word2
+
+result = reduce(add_hyphen, words)
+print(result)
