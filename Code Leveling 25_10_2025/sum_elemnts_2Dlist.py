94dd9da3cef8f04676bbe12f2d303f8b71fc2b67
Abdulwahab Adamson <adamsonoladipupo@gmail.com>
2025-10-26 20:01:40 +0100

    new update

--- Code Leveling 25_10_2025/sum_elemnts_2Dlist.py
+++ Code Leveling 25_10_2025/sum_elemnts_2Dlist.py
@@ -2,13 +2,13 @@ list =  [ [2, 3, 4],  [1, 5, 7],  [4, 6, 8], [5,5,5]]
 
 def sum_2D_list_element(input):
 	count = 0
-	temVar = 0
+	tempVar = 0
 	for item2 in input:
 		for item in item2:
-			temVar +=item
-		input[count] = temVar
+			tempVar +=item
+		input[count] = tempVar
 		count +=1
-		temVar = 0
+		tempVar = 0
 	return input
 
 print(sum_2D_list_element(list))	
