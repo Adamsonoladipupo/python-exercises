f4be38b0556e239164b67da0bda7e3a7fbca8af5
OlaAdams <adamsonoladipupo@gmail.com>
2025-12-26 01:10:35 +0100

    new update

--- MFDS v2.0/python version/test/entities/test_Dispenser.py
+++ MFDS v2.0/python version/test/entities/test_Dispenser.py
@@ -7,5 +7,7 @@ from entities.Fuel import Fuel
 class TestDispenser(TestCase):
     def test_if_dispenser_isEmpty(self):
         dispenser = Dispenser
-        self.assertTrue(dispenser.is_empty())
+        #self.assertTrue(dispenser.is_empty())
+        fuel = Fuel("kerosene", 500, 50)
+        print(fuel)
 
